64286be28b41b39a42d402d60d8fa127eb016c87
Md-sajeeb001 <mohammadsajeeb890@email.com>
2025-02-21 10:57:50 +0600

    added get task from db

--- index.js
+++ index.js
@@ -86,6 +86,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/tasks", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // // Send a ping to confirm a successful connection
